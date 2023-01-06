b72a4a38d25f82f73eb718db559050742cc7ff99
Singh <singh@Singhs-MacBook-Pro.local>
2023-01-06 00:02:53 -0800

    complete navbar and responsiveness with logo

--- main.js
+++ main.js
@@ -8,3 +8,34 @@ function buttonHandler() {
   img.src = `data:image/png;base64,${surprise}`
   surpriseElement.appendChild(img)
 }
+
+/*============================================================================
+Navbar credit from Developer, Évelym S.
+  ============================================================================*/
+
+// Navigation
+// Responsive Toggle Navigation =============================================
+let menuIcon = document.querySelector('.menuIcon');
+let nav = document.querySelector('.overlay-menu');
+
+menuIcon.addEventListener('click', () => {
+  if (nav.style.transform != 'translateX(0%)') {
+    nav.style.transform = 'translateX(0%)';
+    nav.style.transition = 'transform 0.2s ease-out';
+  } else {
+    nav.style.transform = 'translateX(-100%)';
+    nav.style.transition = 'transform 0.2s ease-out';
+  }
+});
+
+
+// Toggle Menu Icon ========================================
+let toggleIcon = document.querySelector('.menuIcon');
+
+toggleIcon.addEventListener('click', () => {
+  if (toggleIcon.className != 'menuIcon toggle') {
+    toggleIcon.className += ' toggle';
+  } else {
+    toggleIcon.className = 'menuIcon';
+  }
+});
